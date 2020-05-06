ff81847319d30708d42644f4f149d9fe7422eb00
peroxy </>
2020-05-06 19:44:46 +0200

    Updated with appsettings changes

--- RsyncBackup/Program.cs
+++ RsyncBackup/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Renci.SshNet;
 using System;
+using System.Linq;
 
 namespace RsyncBackup
 {
@@ -11,19 +12,17 @@ namespace RsyncBackup
             try
             {
                 Console.WriteLine("RSync backup started.");
-                var (host, username, password) = GetSecrets();
+                var (host, username, password, folders) = GetConfiguration();
+                string localIp = NetworkingHelper.GetLocalIPAddress();
 
                 using (var client = new SshClient(host, username, password))
                 {
                     client.Connect();
-                    using var command = client.CreateCommand("command");
+                    using var command = client.CreateCommand("ls -al");
                     var asyncExecute = command.BeginExecute();
                     command.OutputStream.CopyTo(Console.OpenStandardOutput());
                     command.EndExecute(asyncExecute);
                 }
-                
-
-                
 
                 Console.WriteLine("Finished successfully.");
             }
@@ -36,16 +35,18 @@ namespace RsyncBackup
             }
         }
 
-        private static (string host, string username, string password) GetSecrets()
+        private static (string host, string username, string password, string[] folders) GetConfiguration()
         {
             var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
             .AddUserSecrets(typeof(Program).Assembly)
             .Build();
 
             string host = configuration["BackupHostName"];
             string username = configuration["Username"];
             string password = configuration["Password"];
-            
+            string[] folders = configuration.GetSection("DeltaCopyFolderAliases").GetChildren().Select(x => x.Value).ToArray();
+
             if (string.IsNullOrWhiteSpace(host))
             {
                 throw new ArgumentException("Host name cannot be null or whitespace.");
@@ -58,8 +59,12 @@ namespace RsyncBackup
             {
                 throw new ArgumentException("Password cannot be null or whitespace.");
             }
+            if (folders == null || folders.Length == 0)
+            {
+                throw new ArgumentException("DeltaCopy folder aliases have to contain at least 1 folder.");
+            }
 
-            return (host, username, password);
+            return (host, username, password, folders);
         }
     }
 }
