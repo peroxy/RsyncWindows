2eaf0cc9c38f60a664b1a673d11d9bfef22d078c
peroxy </>
2020-05-06 19:44:27 +0200

    Added NetworkingHelper for local ip fetch

---
+++ RsyncBackup/NetworkingHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace RsyncBackup
+{
+    public class NetworkingHelper
+    {
+        /// Source: https://stackoverflow.com/a/6803109
+        /// <summary>
+        /// Gets local user's IP address.
+        /// </summary>
+        /// <param name="_type"></param>
+        /// <returns></returns>
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+    }
+}
